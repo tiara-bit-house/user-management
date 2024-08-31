9a9fbc859054e9d015a8f3efd9069dbbaa63b66c
Kemz <afilfriend@gmail.com>
2024-08-31 14:28:57 +0800

    add read

--- app/read.php
+++ app/read.php
@@ -1,3 +1,17 @@
 <?php
 
 require_once 'database.php';
+
+$sql = "SELECT * FROM users";
+$result = $connection->query($sql);
+
+if ($result->num_rows) {
+    while ($data = $result->fetch_object()) {
+        $name = $data->NAME;
+        $point = $data->POINT;
+
+        echo "$name memiliki $point point. \n";
+    }
+} else {
+    echo "Tidak ada data.";
+}
