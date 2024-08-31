6284b874b58be963146a57d5130129480ec86814
Kemz <afilfriend@gmail.com>
2024-08-31 13:44:50 +0800

    fresh

---
+++ app/database.php
@@ -0,0 +1,9 @@
+<?php
+
+$connection = new mysqli('localhost', 'root', '', 'store');
+
+if ($connection->connect_error) {
+    die("Failed to connect to database : " . $connection->connect_error);
+}
+
+echo "Connect success";
